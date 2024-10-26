657fe2b13258fbb6f95fca61f5fe0c7468e01709
Guido <sercoguido@gmail.com>
2024-10-26 12:36:08 -0300

    Creacion Login

---
+++ SistemaKioscoProyecto/SistemaKioscoProyecto/Login.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SistemaKioscoProyecto
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TbEmail = new System.Windows.Forms.TextBox();
+            this.TbContra = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ErrMail = new System.Windows.Forms.Label();
+            this.ErrContra = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(542, 131);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(217, 83);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TbEmail
+            // 
+            this.TbEmail.Location = new System.Drawing.Point(205, 44);
+            this.TbEmail.Name = "TbEmail";
+            this.TbEmail.Size = new System.Drawing.Size(274, 20);
+            this.TbEmail.TabIndex = 3;
+            // 
+            // TbContra
+            // 
+            this.TbContra.Location = new System.Drawing.Point(205, 81);
+            this.TbContra.Name = "TbContra";
+            this.TbContra.Size = new System.Drawing.Size(274, 20);
+            this.TbContra.TabIndex = 5;
+            this.TbContra.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contraseña";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ErrMail
+            // 
+            this.ErrMail.AutoSize = true;
+            this.ErrMail.Location = new System.Drawing.Point(206, 65);
+            this.ErrMail.Name = "ErrMail";
+            this.ErrMail.Size = new System.Drawing.Size(0, 13);
+            this.ErrMail.TabIndex = 7;
+            this.ErrMail.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // ErrContra
+            // 
+            this.ErrContra.AutoSize = true;
+            this.ErrContra.Location = new System.Drawing.Point(206, 104);
+            this.ErrContra.Name = "ErrContra";
+            this.ErrContra.Size = new System.Drawing.Size(0, 13);
+            this.ErrContra.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 246);
+            this.Controls.Add(this.ErrContra);
+            this.Controls.Add(this.ErrMail);
+            this.Controls.Add(this.TbContra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TbEmail);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TbEmail;
+        private System.Windows.Forms.TextBox TbContra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ErrMail;
+        private System.Windows.Forms.Label ErrContra;
+    }
+}
+
---
+++ SistemaKioscoProyecto/SistemaKioscoProyecto/Login.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using dominio;
+using Negocio;
+
+namespace SistemaKioscoProyecto
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Llamar al método para cargar los datos al cargar el formulario
+            CargarMails();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string EmailCargado = TbEmail.Text;
+            string ContraCargada = TbContra.Text;
+
+                int resultado = VerificarEmailYContrasena(EmailCargado, ContraCargada);
+
+                switch (resultado)
+                {
+                    case 0:
+                        ErrMail.Text = "El mail ingresado no se encuentra en nuestra Base de datos.";
+                    break;
+                    case 1:
+                        ErrMail.Text = "";
+                        ErrContra.Text = "Contraseña no coincide.";
+                        break;
+                    case 2:
+                    Principal principalForm = new Principal();
+
+                    // Muestra el formulario Principal
+                    principalForm.Show();
+
+                    break;
+                
+            }
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //////////////////////////////////ESTO VA EN NEGOCIO, SACAR DE ACÁ
+        private void CargarMails()
+        {
+            // Cadena de conexión a la base de datos
+            string connectionString = "server =.\\SQLEXPRESS; database = BBDD_KioscoLab3; integrated security = true";
+
+            // Consulta SQL para obtener todos los registros de la tabla Usuario
+            string query = "SELECT Email FROM Usuario";
+
+            // Usamos SqlConnection y SqlDataAdapter para conectarnos y obtener los datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Usamos SqlDataAdapter para ejecutar la consulta y llenar un DataTable
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Mostrar los datos en el DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        //ESTO VA EN NEGOCIO, SACAR DE ACÁ
+        private void VerificaExistenciaMail()
+        {
+            // Cadena de conexión a la base de datos
+            string connectionString = @"Server=DESKTOP-D071EVB\SQLEXPRESS;Database=BBDD_KioscoLab3;Integrated Security=True;";
+
+            // Consulta SQL para obtener todos los registros de la tabla Usuario
+            string query = "SELECT Email FROM Usuario";
+
+            // Usamos SqlConnection y SqlDataAdapter para conectarnos y obtener los datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Usamos SqlDataAdapter para ejecutar la consulta y llenar un DataTable
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Mostrar los datos en el DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+                }
+            }
+        }
+
+        public int VerificarEmailYContrasena(string email, string contrasenia)
+        {
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=BBDD_KioscoLab3; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+
+                comando.CommandText = "SELECT CASE " +
+                                      "WHEN Email = @Email AND Contrasenia = @Contrasenia THEN 2 " +
+                                      "WHEN Email = @Email THEN 1 " +
+                                      "ELSE 0 " +
+                                      "END " +
+                                      "FROM Usuario " +
+                                      "WHERE Email = @Email;";
+                comando.Parameters.AddWithValue("@Email", email);
+                comando.Parameters.AddWithValue("@Contrasenia", contrasenia);
+                comando.Connection = conexion;
+
+                conexion.Open();
+
+                object resultObj = comando.ExecuteScalar();
+
+                if (resultObj != null)
+                {
+                    int result = (int)resultObj;
+                    return result;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conexion.State == System.Data.ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+        }
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+        
+}

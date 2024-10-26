11199f6e3697c092a0351f72179508af6772e1df
FrancoBonzi <bon.franco12@gmail.com>
2024-10-26 13:25:35 -0300

    commit: MarcaNegocio,CategoriaNegocio, NacionalidadNegocio y class Ciudad

---
+++ SistemaKioscoProyecto/Negocio/CategoriaNegocio.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista=new List<Categoria>();
+            AccesoDatos datos= new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Id_Categoria,Nombre from Categoria");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.IdCategoria = (int)datos.Lector["Id_Categoria"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+
+                    lista.Add(aux);
+                }
+                datos.cerrarConexion();
+                return lista;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Agregar(Categoria nuevo)
+        {
+            AccesoDatos datos= new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("insert into Categoria (Nombre) values (@nombre)");
+                datos.setearParametro("@nombre", nuevo.Nombre);
+                datos.ejecutarAccion();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Modificar(Categoria modificar)
+        {
+            AccesoDatos datos= new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("UPDATE Categoria set Nombre=@nombre where Id_Categoria=@id");
+                datos.setearParametro("@nombre", modificar.Nombre);
+                datos.setearParametro("@id", modificar.IdCategoria);
+
+                datos.ejecutarAccion();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}

6afd0d6e869c21de54f6863bcce115e45652b281
Guido <sercoguido@gmail.com>
2024-11-04 15:12:50 -0300

    Vista

--- SistemaKioscoProyecto/SistemaKioscoProyecto/Articulos.Designer.cs
+++ SistemaKioscoProyecto/SistemaKioscoProyecto/Articulos.Designer.cs
@@ -32,13 +32,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.Dgv_Articulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Articulos)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightGreen;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(42, 48);
+            this.button1.Location = new System.Drawing.Point(42, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(710, 78);
             this.button1.TabIndex = 0;
@@ -50,7 +52,7 @@
             // 
             this.button2.BackColor = System.Drawing.Color.SkyBlue;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button2.Location = new System.Drawing.Point(42, 132);
+            this.button2.Location = new System.Drawing.Point(42, 103);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(710, 78);
             this.button2.TabIndex = 1;
@@ -62,7 +64,7 @@
             // 
             this.button3.BackColor = System.Drawing.Color.Salmon;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button3.Location = new System.Drawing.Point(42, 216);
+            this.button3.Location = new System.Drawing.Point(42, 187);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(710, 78);
             this.button3.TabIndex = 2;
@@ -74,7 +76,7 @@
             // 
             this.button4.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button4.Location = new System.Drawing.Point(42, 300);
+            this.button4.Location = new System.Drawing.Point(42, 271);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(710, 78);
             this.button4.TabIndex = 3;
@@ -82,11 +84,21 @@
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // Dgv_Articulos
+            // 
+            this.Dgv_Articulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Articulos.Location = new System.Drawing.Point(26, 368);
+            this.Dgv_Articulos.Name = "Dgv_Articulos";
+            this.Dgv_Articulos.Size = new System.Drawing.Size(762, 219);
+            this.Dgv_Articulos.TabIndex = 4;
+            this.Dgv_Articulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // Articulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 599);
+            this.Controls.Add(this.Dgv_Articulos);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -94,6 +106,7 @@
             this.Name = "Articulos";
             this.Text = "Articulos";
             this.Load += new System.EventHandler(this.Articulos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Articulos)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -104,5 +117,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView Dgv_Articulos;
     }
 }
--- SistemaKioscoProyecto/SistemaKioscoProyecto/Articulos.cs
+++ SistemaKioscoProyecto/SistemaKioscoProyecto/Articulos.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Negocio;
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace SistemaKioscoProyecto
@@ -36,7 +38,37 @@ namespace SistemaKioscoProyecto
 
         private void Articulos_Load(object sender, EventArgs e)
         {
+            CargarArticulos();
+        }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        public void CargarArticulos()
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT * FROM view_DetallesArticulo", System.Data.CommandType.Text);
+
+                DataTable tabla = new DataTable();
+                datos.ejecutarLectura();
+                tabla.Load(datos.Lector);
+
+                Dgv_Articulos.DataSource = tabla;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al cargar los artículos: " + ex.Message);
+            }
+            finally
+            {
+                datos.cerrarConexion(); // Cerrar la conexión después de la consulta
+            }
         }
     }
 }
